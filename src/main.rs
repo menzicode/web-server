3b4fdcd220907dd4f6886c2e64842f1d45f72de3
Matt Menzi <mhmenzi@gmail.com>
2024-03-26 23:35:44 -0400

    Added listener

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::net::TcpListener;
-
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+}
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
@@ -7,4 +9,8 @@ fn main() {
 
         println!("Connection established!");
     }
+}
+
+fn handle_connection(mut stream:: TcpStream) {
+    
 }
