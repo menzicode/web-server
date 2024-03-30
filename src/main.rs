301bc0b3f977e92be4b9a5be84ea2503afff2cf3
Matt Menzi <mhmenzi@gmail.com>
2024-03-30 16:15:42 -0400

    Returns HTML

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -20,7 +21,15 @@ fn handle_connection(mut stream: TcpStream) {
        .take_while(|line| !line.is_empty())
        .collect();
 
-   let response = String::from("HTTP/1.1 200 OK\r\n\r\nHello");
-   stream.write_all(response.as_bytes()).expect("Err");
+   let status_line = "HTTP/1.1 200 OK";
+   let contents = fs::read_to_string("hello.html").unwrap();
+   let length = contents.len();
+
+   let response = 
+       format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+   stream.write_all(response.as_bytes()).unwrap();
    println!("Request: {:#?}", http_request);
+   println!("Response: {:#?}", response.to_string());
 }
+
