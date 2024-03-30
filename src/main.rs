98c37b48731e2d317ff173bc7959f27b19923477
Matt Menzi <mhmenzi@gmail.com>
2024-03-30 13:50:56 -0400

    Read request

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-}
+};
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
 }
 
-fn handle_connection(mut stream:: TcpStream) {
-   mememem 
+fn handle_connection(mut stream: TcpStream) {
+   let buf_reader = BufReader::new(&mut stream);
+   let http_request: Vec<_> = buf_reader
+       .lines()
+       .map(|result| result.unwrap())
+       .take_while(|line| !line.is_empty())
+       .collect();
+
+   println!("Request: {:#?}", http_request);
+   let _hello = String::from("Hello!");
+   stream.write(&[1]).expect("Fail");
 }
